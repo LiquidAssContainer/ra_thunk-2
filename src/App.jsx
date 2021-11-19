92dd3b09cd50e3f681b10f2b980318fb600cccb1
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2021-11-19 17:03:15 +0500

    Add yarn.lock, replace Browse- with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Redirect,
   Link,
